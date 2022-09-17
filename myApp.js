85e5d7351804bea39e94f4119d4bfddd86b54322
DylanMcComas <dylan.mccomas@me.com>
2022-09-17 14:02:38 -0400

    feat: serve JSON on a specific route

--- myApp.js
+++ myApp.js
@@ -3,11 +3,17 @@ let app = express();
 
 let absolutePath = __dirname + "/views/index.html"
 
+let data = {"message": "Hello json"};
+
 console.log("Hello World");
 console.log("Hello, Dylan");
 
 app.get("/", (req, res) => {
     res.sendFile(absolutePath);
+});
+
+app.get("/json", (req, res) => {
+    res.json(data);
 });
 
 app.use("/public", express.static(__dirname + "/public"));
